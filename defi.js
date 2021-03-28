6a4a610441e08bbd0957f828e5d410d7096d4ef8
bikibird <jenny@bikibird.com>
2021-03-28 18:57:09 -0500

    Updated

--- defi.js
+++ defi.js
@@ -1,13 +1,16 @@
 function convert(midi,granularity=2)
 {
-	const channels=[[]]
+	const dec2hex=(n)=>(0x10000+n).toString(16).substr(-4)
 	const tracks=[]
 	var ppq=midi.header.ppq
 	var resolution=ppq/granularity
-	var tempoIndex=0
-	var channelIndex=0
-	var tempo=120
 	var tempos=midi.header.tempos
+	var pico8 =
+	{
+		effects:{"None":0, "Slide":4096, "Vibrato":8192, "Drop":12388, "Fade In":16384, "Fade Out":20480, "Fast Arpeggio":24576, "Slow Arpeggio":28762},
+		volumes:[0, 512, 1024, 1536, 2048, 2560, 3072, 3584],
+		instruments:{"Triangle": 0, "Tilted Saw": 64, "Saw": 128, "Square": 192, "Pulse": 256, "Organ": 320, "Noise": 384, "Phaser" :448, "Custom 0": 32768, "Custom 1": 32832,"Custom 2": 32896,"Custom 3": 32960,"Custom 4": 33024,"Custom 5": 33088,"Custom 6": 33152,"Custom 7": 33216}
+	}
 	//Check for simultaneous notes and split into multiple tracks
 	midi.tracks.filter(track=>track.notes.length>0).forEach(track=>
 	{
@@ -67,6 +70,7 @@ function convert(midi,granularity=2)
 		tempo.channels=[[]]
 		var channelIndex=0
 		speed=Math.floor((7200/ppq)*resolution/tempo.bpm +.5)
+		tempo.speed=speed
 		tracks.forEach(track=>
 		{
 			while (track.trackIndex<track.length && (!tempo.stop || track[track.trackIndex].ticks<tempo.stop))
@@ -78,10 +82,11 @@ function convert(midi,granularity=2)
 					tempo.channels[channelIndex].push(
 					{
 						pitch:track[track.trackIndex].midi-36,
-						speed:speed,
+						//speed:speed,
 						beats:Math.floor(track[track.trackIndex].durationTicks/resolution +.5),
 						volume:Math.floor(7*track[track.trackIndex].velocity+.5),
-						start:track[track.trackIndex].ticks
+						notes:[]
+						//start:track[track.trackIndex].ticks
 					})
 				}	
 				track.trackIndex++
@@ -92,6 +97,24 @@ function convert(midi,granularity=2)
 		tempo.channels=tempo.channels.filter(channel=>channel.length>0)		
 	})
 
+	tempos.forEach(tempo=>
+	{
+		tempo.channels.forEach(channel=>
+		{
+			var sfx=""
+			channel.forEach(note=>
+			{
+				
+				for (let i = 0; i< note.beats; i++)
+				{
+					//sfx=sfx+dec2hex(note.pitch +pico8.volumes[note.volume])
+					sfx=sfx+dec2hex(note.pitch*256)
+				}	
+				channel.sfx=sfx
+			})
+		})
+	})	
+	console.log(tempos)
 }
 
 
